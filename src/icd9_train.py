d0e58bec844d1b3faf3941cf8c06eb814fd29a05
Anant <anant718@gmail.com>
2017-11-12 17:48:10 -0500

    fix

--- src/icd9_train.py
+++ src/icd9_train.py
@@ -49,7 +49,7 @@ def basicstats(patients):
     print "# of unique target icds: %d"%len(list(set(target_icds)))
     print "Top occuring: ", Counter(target_icds).most_common(20)
     num_seq = sorted([len(enc.keys())-1 for p, enc in patients.iteritems()])
-    print "FD:", np.histogram(num_seq, bins=[0, 2, 5,10,20,50,100,500,1000,10000]) 
+    print "FD:", np.histogram(num_seq, bins=[0, 1, 2, 5,10,20,50,100,500,1000,10000]) 
 
 def read_icd9_embeddings(embedding_path):
     token2idx = {'<PAD>': 0, '<UNK>': 1}
@@ -106,7 +106,7 @@ def padding_collation(batch):
     batch_list, label_list = [], []
     max_seq_len = np.max([len(datum[0]) for datum in batch])
     for datum in batch:
-        padded_vec = datum[0] + [0 for i in range(max_seq_len-len(datum[0]))]
+        padded_vec = [0 for i in range(max_seq_len-len(datum[0]))] + datum[0]
         batch_list.append(padded_vec)
         label_list.append(datum[1])
     return [batch_list, label_list]
@@ -116,7 +116,7 @@ import torch.nn as nn
 from torch.autograd import Variable
 import torch.nn.functional as F
 class LSTMModel(nn.Module):
-    def __init__(self, vocab_size, embed_dim, hidden_dim, batch_size):
+    def __init__(self, vocab_size, embed_dim, hidden_dim, batch_size, labels):
         super(LSTMModel, self).__init__()
         self.embed_dim = embed_dim
         #self.embed = nn.Embedding(vocab_size, embed_dim)
@@ -135,18 +135,18 @@ class LSTMModel(nn.Module):
         return x
 
 class Trainer():
-    def __init__(self, train_loader, embeddings, token2idx, idx2token, test_loader, batch_size):
+    def __init__(self, train_loader, embeddings, token2idx, idx2token, test_loader, batch_size, labels):
         self.train_loader = train_loader
         self.test_loader = test_loader
         self.embeddings = embeddings
         self.token2idx = token2idx
         self.idx2token = idx2token
         self.batch_size = batch_size
-        self.num_epochs = 1
+        self.num_epochs = 100
         self.embedding_size = 300
         self.hidden_dim = 100
         self.lr = 1e-3
-        self.model = LSTMModel(len(self.token2idx.keys()), self.embedding_size, self.hidden_dim, self.batch_size)
+        self.model = LSTMModel(len(self.token2idx.keys()), self.embedding_size, self.hidden_dim, self.batch_size, labels)
         self.crit = nn.CrossEntropyLoss()
         self.opti = torch.optim.Adam(self.model.parameters(), lr=self.lr, betas=(0.5, 0.999))
         self.avg_pad = 0
@@ -155,7 +155,11 @@ class Trainer():
 
     def aggregate_embeddings(self, batch_x):
         batch_emb = np.ndarray((self.batch_size, len(batch_x[0]), self.embedding_size))
+        #skipped = 0
         for i, enclist in enumerate(batch_x):
+            #if np.sum([1 for _ in enclist if _ == 0]) > len(enclist)//2:
+            #    skipped +=1
+            #    continue
             for j, enc in enumerate(enclist):
                 self.avg_tokens += 1
                 if enc == 0:
@@ -189,24 +193,33 @@ class Trainer():
                 
                 x = self.model(x, hidden)
                 loss = self.crit(x, Variable(batch_y.view(-1)))
+                #print ">>>>>>>>>>"
+                #print x.size()
+                #print x[0], batch_y.view(-1)[0]
                 loss.backward()
                 self.opti.step()
-
+                for param in self.model.parameters():
+                    print param[0]
+                    break
                 if step % 100 == 0:
+                    print batch[0]
+                    print torch.max(x.data, 1)[1] 
+                    print batch_y
                     print "average paddings:", np.sum(self.avg_pad)/(np.sum(self.avg_tokens)*1.0)
                     self.avg_pad, self.avg_tokens = 0,0
                     self.model.eval()
                     val_acc, val_loss = self.evaluate()
                     self.model.train()
-                    print("Step: %d, Loss: %.4f, Validation Acc: %.2f"%(step, loss.data[0], val_acc))
+                    print("Step: %d, Loss: %.4f, Validation Acc: %.2f, Validation Loss: %.2f"%(step, loss.data[0], val_acc, val_loss))
                     step_log.append(step)
                     loss_log.append(loss.data[0])
                     val_acc_log.append(val_acc)
                     val_loss_log.append(val_loss)
+
+                    self.lr *= 0.9
+                    self.opti = torch.optim.Adam(self.model.parameters(), lr=self.lr, betas=(0.5, 0.999))
                 step += 1
-            self.lr *= 0.9
-            self.opti = torch.optim.Adam(self.model.parameters(), lr=self.lr, betas=(0.5, 0.999))
-        self.plot(step_log, loss_lof, val_acc_log, val_loss_log)
+        self.plot(step_log, loss_log, val_acc_log, val_loss_log)
 
 
     def evaluate(self):
@@ -224,7 +237,7 @@ class Trainer():
             _, predicted = torch.max(x.data, 1)
             total += batch_y.size(0)
             correct += (predicted == batch_y).sum()
-            avg_loss += self.crit(x, Variable(batch_y.view(-1))).data[0]
+            avg_loss = self.crit(x, Variable(batch_y.view(-1))).data[0]
                 
         return (correct / float(total), np.mean(avg_loss))
 
@@ -247,6 +260,11 @@ class Trainer():
 
 if __name__ == "__main__":
     base_path = '/media/disk3/disk3/'
+
+    #patients, pat_adm_id = build_encounter_seq(os.path.join(base_path, 'mimic3/DIAGNOSES_ICD.csv'))
+    #basicstats(patients)
+    #exit()
+
     embeddings, token2idx, idx2token = read_icd9_embeddings(os.path.join(base_path, 'claims_codes_hs_300.txt'))
 
     labels = ['4019','41401','42731','4280','25000','2724','5849','V053','51881','V290','2720','53081','5990','2859','2449','V3000','2851','486','2762','496']
@@ -255,26 +273,34 @@ if __name__ == "__main__":
     if os.path.exists(datapath):
         data = pickle.load(open(datapath, 'r'))['data']
     else:
-        patients, pat_adm_id, token2idx = build_encounter_seq(os.path.join(base_path, 'mimic3/DIAGNOSES_ICD.csv'))
-        #POC_of_seq(patients, pat_adm_id)
+        patients, pat_adm_id = build_encounter_seq(os.path.join(base_path, 'mimic3/DIAGNOSES_ICD.csv'))
         basicstats(patients)
-        db = ICD9DataBuilder(patients, labels, embeddings, token2idx)
+        db = ICD9DataBuilder(patients, labels, token2idx)
         db.label_data()
         f = open(datapath, 'w')
         pickle.dump({'data': db.data}, f)
         f.close()
         data = db.data
-
+    '''
+    _bin = {1:0, 2:0}
+    for i in [_['X'] for _ in data]:
+        if len(i)>0:
+            _bin[1] +=1
+        elif len(i)>1:
+            _bin[2] +=1
+    print _bin
+    exit()
+    '''
     random.shuffle(data)
 
     _break = int(0.8*len(data))
     train_data = ICD9Dataset(data[:_break])
     test_data = ICD9Dataset(data[_break:])
-    batch_size = 64
+    batch_size = 5
     train_loader = torch.utils.data.DataLoader(dataset=train_data, shuffle=True, 
                                 batch_size=batch_size,collate_fn=padding_collation)
     test_loader = torch.utils.data.DataLoader(dataset=test_data, shuffle=True, 
                                 batch_size=batch_size,collate_fn=padding_collation)
 
-    trainer = Trainer(train_loader, embeddings, token2idx, idx2token, test_loader, batch_size) 
+    trainer = Trainer(train_loader, embeddings, token2idx, idx2token, test_loader, batch_size, labels) 
     trainer.train()
