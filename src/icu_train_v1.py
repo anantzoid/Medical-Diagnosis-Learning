bef8cd3dc5d83757b0aea6f4edfb5d9288bdcc7b
Anant <anant718@gmail.com>
2017-11-17 01:48:36 -0500

    tensor avg on the fly

--- src/icu_train_v1.py
+++ src/icu_train_v1.py
@@ -18,7 +18,7 @@ use_cuda = False#torch.cuda.is_available()
 batch_size = 1
 num_workers = 1
 hidden_dim = 400
-
+random.seed(10)
 
 # In[42]:
 
@@ -105,7 +105,7 @@ class LSTMModel(nn.Module):
         self.embed.weight = nn.Parameter(torch.from_numpy(pretrained).float())
 
         #Attention!!!
-        self.lstm = nn.LSTM(embed_dim, hidden_dim, bidirectional=True)
+        self.lstm = nn.LSTM(embed_dim, hidden_dim, bidirectional=False)
         self.lin = nn.Linear(hidden_dim, num_labels)
 
     def init_hidden(self):
@@ -119,10 +119,10 @@ class LSTMModel(nn.Module):
         # seqlen x batch x emb_dim
 
         # batch x seq_len x list of idx  -> batch x seq_len x embed_size
-        avg_embeds = []
-        for b in range(batch_size):
-            avg_embeds.append(torch.mean(embed(x[b, :, :]), dim=1))
-        x = torch.cat(avg_embeds, dim=0)
+        #avg_embeds = []
+        #for b in range(batch_size):
+        #    avg_embeds.append(torch.mean(embed(x[b, :, :]), dim=1))
+        #x = torch.cat(avg_embeds, dim=0)
         x = torch.transpose(x, 1, 0)
         x, _hidden  = self.lstm(x, hidden)
         x = x[-1, :, :].view(self.batch_size, -1)
@@ -136,7 +136,7 @@ print "Reading data..."
 _time = time.time()
 pretrained, token2idx = read_embeddings(os.path.join(base_path, 'ri-3gram-400-tsv/filtered_embeddings.tsv'))
 traindata, valdata = read_data_dump(os.path.join(base_path, 'notes_dump_cleaned.pkl'), token2idx)
-label_map = get_labels(os.path.join(base_path, 'icd9_ccs.csv'))
+label_map = get_labels(os.path.join(base_path, 'labels.txt'))
 print "Preprocessing done in %.2f"%((time.time()-_time)/60)
 
 
@@ -150,12 +150,11 @@ if use_cuda:
     model.cuda()
     crit.cuda()
 
-train_loader = torch.utils.data.DataLoader(dataset=Dataloader(traindata), batch_size=batch_size, shuffle=True, 
-                                                           num_workers=num_workers)
+#train_loader = torch.utils.data.DataLoader(dataset=Dataloader(traindata), batch_size=batch_size, shuffle=True, 
+#                                                           num_workers=num_workers)
 #val_loader = torch.utils.data.DataLoader(dataset=Dataloader(valdata), batch_size=batch_size, shuffle=False) 
 
 # In[147]:
-
 def evaluate(model, loader, batch_size, label_map):
     correct = 0
     total = 0
@@ -183,7 +182,9 @@ def evaluate(model, loader, batch_size, label_map):
         x = model(x, hidden)
         _, predicted = torch.max(x.data, 1)
         total += batch_y.size(0)
-        correct += (predicted == batch_y).sum()
+        print predicted
+        print batch_y
+        correct += (predicted == batch_y.data).sum()
         break
     return correct / float(total)
 
@@ -198,42 +199,36 @@ val_acc_log = []
 train_acc_log = []
 train_acc = []
 torch.manual_seed(1)
+
+
 for num_ep in range(num_epochs):
-    for i in train_loader:
-        if len(i[0]) != batch_size:
-            continue
+    #for i in train_loader:
+    for i,dp in enumerate(traindata[:batch_size]):
+        #if len(dp) != batch_size:
+        #    continue
+
+        mnl =  np.max([len(_) for _ in dp['notes']])
+        padded_notes=[]
+        for _ in dp['notes']:
+            padded_notes.append([0]*(mnl-len(_))+_)
+        batch_x = torch.mean(model.embed(Variable(torch.from_numpy(np.array(padded_notes)).long())), dim=1).unsqueeze(0)
+
+        batch_y = Variable(torch.from_numpy(np.array([label_map[dp['labels']['icd'][0]]])).long().view(batch_size))
+        
         model.zero_grad()
-        for _ in i[0][0]:
-            print len(_)
-        print np.array(i[0][0]).shape 
-        exit()
-        seq_len = len(i[0][0])
-        #batch_x = np.ndarray((batch_size, seq_len, 400))
-        batch_x = np.array(i[0])
-        for bs, adm in enumerate(i[0]):
-            for idx, note in enumerate(adm):
-                note_vec = []
-                for token in note:
-                    note_vec.append(pretrained.get(re.sub(r'[^\w\s]','', token), pretrained['unknown']))
-                
-                batch_x[bs, idx, :] = np.mean(note_vec)  
-                # batch_x[bs, idx, :] = np.mean([pretrained.get(re.sub(r'[^\w\s]','', token), pretrained['unknown']) for token in note], axis=0)
-        batch_x = torch.from_numpy(batch_x).float()
-        batch_y = torch.from_numpy(np.array([label_map[_] for _ in i[1]])).long().view(batch_size)
+        #x = Variable(batch_x)
+
         if use_cuda:
             batch_x, batch_y = batch_x.cuda(), batch_y.cuda()
-
-        x = Variable(batch_x)
         hidden = model.init_hidden()
-        x = model(x, hidden)
-
-        loss = crit(x, Variable(batch_y))
+        x = model(batch_x, hidden)
+        loss = crit(x, batch_y)
         loss.backward()
         opti.step()
 
         _, predicted = torch.max(x.data, 1)
         total = batch_y.size(0)
-        correct = (predicted == batch_y).sum()
+        correct = (predicted == batch_y.data).sum()
         train_acc.append(correct / float(total))
 
         if step % 100 == 0:
