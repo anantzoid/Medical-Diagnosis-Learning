b61fe5fcc5daa120ef242f292533a9455818bce3
Laura Graesser <lhg256@cuda2.cims.nyu.edu>
2017-11-28 12:40:32 -0500

    Adding micro F1 score and stopword removal to training script

--- src/build_datasets_utils.py
+++ src/build_datasets_utils.py
@@ -3,6 +3,48 @@ import numpy as np
 import torch
 import torch.utils.data as data
 
+ENGLISH_STOP_WORDS = frozenset([
+    "a", "about", "above", "across", "after", "afterwards", "again", "against",
+    "all", "almost", "alone", "along", "already", "also", "although", "always",
+    "am", "among", "amongst", "amoungst", "amount", "an", "and", "another",
+    "any", "anyhow", "anyone", "anything", "anyway", "anywhere", "are",
+    "around", "as", "at", "back", "be", "became", "because", "become",
+    "becomes", "becoming", "been", "before", "beforehand", "behind", "being",
+    "below", "beside", "besides", "between", "beyond", "bill", "both",
+    "bottom", "but", "by", "call", "can", "cannot", "cant", "co", "con",
+    "could", "couldnt", "cry", "de", "describe", "detail", "do", "done",
+    "down", "due", "during", "each", "eg", "eight", "either", "eleven", "else",
+    "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone",
+    "everything", "everywhere", "except", "few", "fifteen", "fifty", "fill",
+    "find", "fire", "first", "five", "for", "former", "formerly", "forty",
+    "found", "four", "from", "front", "full", "further", "get", "give", "go",
+    "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter",
+    "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his",
+    "how", "however", "hundred", "i", "ie", "if", "in", "inc", "indeed",
+    "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter",
+    "latterly", "least", "less", "ltd", "made", "many", "may", "me",
+    "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly",
+    "move", "much", "must", "my", "myself", "name", "namely", "neither",
+    "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone",
+    "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on",
+    "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our",
+    "ours", "ourselves", "out", "over", "own", "part", "per", "perhaps",
+    "please", "put", "rather", "re", "same", "see", "seem", "seemed",
+    "seeming", "seems", "serious", "several", "she", "should", "show", "side",
+    "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone",
+    "something", "sometime", "sometimes", "somewhere", "still", "such",
+    "system", "take", "ten", "than", "that", "the", "their", "them",
+    "themselves", "then", "thence", "there", "thereafter", "thereby",
+    "therefore", "therein", "thereupon", "these", "they", "thick", "thin",
+    "third", "this", "those", "though", "three", "through", "throughout",
+    "thru", "thus", "to", "together", "too", "top", "toward", "towards",
+    "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us",
+    "very", "via", "was", "we", "well", "were", "what", "whatever", "when",
+    "whence", "whenever", "where", "whereafter", "whereas", "whereby",
+    "wherein", "whereupon", "wherever", "whether", "which", "while", "whither",
+    "who", "whoever", "whole", "whom", "whose", "why", "will", "with",
+    "within", "without", "would", "yet", "you", "your", "yours", "yourself",
+    "yourselves"])
 
 class FlatData(data.Dataset):
     def __init__(self, data, word_2_idx, label_map):
@@ -57,7 +99,13 @@ def build_dictionary(train_data, PADDING):
         note = d[1]
         for sent in note:
             vocab.extend(sent)
-    vocab = [_[0] for _ in list(Counter(vocab).most_common()) if _[0] is not 'UNK']
+    vocab = [_[0] for _  in list(Counter(vocab).most_common())]
+    try:
+      print("Deleting UNK")
+      idx = vocab.index('UNK')
+      del vocab[idx]
+    except:
+      print("No UNK found")
     vocab = [PADDING, 'UNK'] + vocab
     print("Size of vocabulary: {}".format(len(vocab)))
     print("Top 100 words...")
@@ -70,3 +118,26 @@ def build_dictionary(train_data, PADDING):
 def build_label_map(labels):
     label_indices = dict(zip(labels, range(len(labels))))
     return label_indices
+
+def remove_stopwords(data):
+    avg_length_before = 0
+    avg_length_after = 0
+    max_b = 0
+    max_a = 0
+    for d in data:
+      note = d[1]
+      n = [[tok for tok in sent if tok in ENGLISH_STOP_WORDS] for sent in note]
+      len_b = sum([1 for sent in note for tok in sent])
+      len_a = sum([1 for sent in n for tok in sent])
+      avg_length_before += len_b
+      avg_length_after += len_a
+      if len_b > max_b:
+        max_b = len_b
+      if len_a > max_a:
+        max_a = len_a
+      d[1] = n
+    print("Avg length before: {}, avg length after removing stopwords: {}".format(
+        avg_length_before / float(len(data)), avg_length_after / float(len(data))))
+    print("Max length before: {}, max length after removing stopwords: {}".format(
+       max_b, max_a)) 
+    return data
--- src/train.py
+++ src/train.py
@@ -8,21 +8,24 @@ import torch.utils.data as data
 import torch.nn as nn
 from torch.autograd import Variable
 from build_datasets_utils import *
-from evaluate import *
 from models import LSTMModel
 PADDING = '<PAD>'
 num_workers = 1
 
 parser = argparse.ArgumentParser(description='MIMIC III training script')
-parser.add_argument('--datafolder', type=str, default='/Users/lauragraesser/Documents/NYU_Courses/medical_data', help="folder where data is located")
+parser.add_argument('--datafolder', type=str, default='/misc/vlgscratch2/LecunGroup/laura/medical_notes/processed_data', help="folder where data is located")
 parser.add_argument('--dataset', type=str, default='10codesL5_UNK_content_4', help="dataset prefix")
 parser.add_argument('--model', type=str, default='LSTM', help="Which model to use")
+parser.add_argument('--modelfolder', type=str, default='/misc/vlgscratch2/LecunGroup/laura/medical_notes/models', help="Where to save the models")
 parser.add_argument('--experiment', type=str, default='test', help="experiment name")
 parser.add_argument('--epochs', type=int, default=10, help="Number of epochs")
 parser.add_argument('--batchsize', type=int, default=32, help="Batch size")
 parser.add_argument('--embeddim', type=int, default=50, help="Embedding dim")
 parser.add_argument('--hiddendim', type=int, default=50, help="Hidden dim")
+parser.add_argument('--lr', type=float, default=0.001, help="Learning rate")
 parser.add_argument('--cuda', type=int, default=0, help="Batch size")
+parser.add_argument('--gpu', type=int, default=0, help="Which gpu to use")
+parser.add_argument('--stop', type=int, default=0, help="Whether to remove stopwords")
 args = parser.parse_args()
 print(args)
 print()
@@ -31,6 +34,10 @@ train_data = pickle.load(open(os.path.join(args.datafolder, args.dataset + "_tra
 valid_data = pickle.load(open(os.path.join(args.datafolder, args.dataset + "_valid_data.pkl"), 'rb'))
 labels = pickle.load(open(os.path.join(args.datafolder, args.dataset + "_labels.pkl"), 'rb'))
 print("Training and validation data loaded")
+if args.stop:
+  print("Removing stopwords...")
+  train_data = remove_stopwords(train_data)
+  valid_data = remove_stopwords(valid_data)
 (word_2_idx, vocab) = build_dictionary(train_data, PADDING)
 label_map = build_label_map(labels)
 print("Labels: {}\n, label map: {}".format(labels, label_map))
@@ -54,17 +61,72 @@ else:
     # To fix with more models
     model = LSTMModel(len(vocab), args.embeddim, args.hiddendim, label_map, args.batchsize, args.cuda)
 crit = nn.BCEWithLogitsLoss()
-params = list(model.parameters())
-opti = torch.optim.Adam(params)
 if args.cuda:
     print("Using cuda")
+    torch.cuda.device(args.gpu)
     model.cuda()
-    opti.cuda()
+    crit.cuda()
 else:
     print("Using CPU only")
+params = list(model.parameters())
+opti = torch.optim.Adam(params)
+
+def evaluate(model, loader, crit, cuda, bs, num_labels):
+    data_size = len(loader)
+    avg_length = 0
+    correct = 0
+    total = 0
+    total_loss = 0
+    true_pos = np.zeros(num_labels)
+    true_neg = np.zeros(num_labels)
+    false_pos = np.zeros(num_labels)
+    false_neg = np.zeros(num_labels)
+    last_pred = None
+    last_y = None
+    for j, (data, labels) in enumerate(loader):
+        x = Variable(data)
+        y = Variable(labels)
+        if cuda:
+            x = x.cuda()
+            y = y.cuda()
+        if x.size(0) != bs:
+            continue
+        avg_length += x.data.size(1)
+        model.eval()
+        out = model(x)
+        out = out.double()
+        loss = crit(out, y)
+        total_loss += loss.data[0]
+        predicted = out.data > 0.5
+        predicted = predicted.int()
+        last_pred = predicted
+        y = y.data.int()
+        last_y = y
+        total += data.size(0)
+        correct += (predicted == y).sum()
+        predicted = predicted.cpu().numpy()
+        y = y.cpu().numpy()
+        for k in range(y.shape[1]):
+          true_pos[k] += np.sum((predicted[:,k][np.where(y[:,k] == 1)] == y[:,k][np.where(y[:,k] == 1)]))
+          true_neg[k] += np.sum((predicted[:,k][np.where(y[:,k] == 0)] == y[:,k][np.where(y[:,k] == 0)]))
+          false_pos[k] += np.sum((predicted[:,k][np.where(y[:,k] == 1)] != y[:,k][np.where(y[:,k] == 1)]))
+          false_neg[k] += np.sum((predicted[:,k][np.where(y[:,k] == 0)] != y[:,k][np.where(y[:,k] == 0)]))
+        if j % 50 == 0:
+            print("Processed {} batches".format(j+1))
+    micro_precision = np.sum(true_pos) / (np.sum(true_pos) + np.sum(false_pos))
+    micro_recall = np.sum(true_pos) / (np.sum(true_pos) + np.sum(false_neg))
+    micro_F = (2 * micro_precision * micro_recall) / (micro_precision + micro_recall)
+    print("correct: {}, loss = {}, data_size: {}, prediction egs: {}, out egs: {}".format(
+              correct / float(total), total_loss / float(data_size), data_size * bs, last_pred[:5], last_y[:5]))
+    print("True pos: {}, True neg: {}, False pos: {}, False neg: {}".format(
+              np.sum(true_pos), np.sum(true_neg), np.sum(false_pos), np.sum(false_neg)))
+    print("Micro precision: {:4f}, micro recall: {:4f}, micro F1: {:4f}".format(micro_precision, micro_recall, micro_F)) 
+
 
 for i in range(args.epochs):
     data_size = len(train_loader)
+    avg_length = 0
+    num_labels = None
     for j, (data, labels) in enumerate(train_loader):
         x = Variable(data)
         y = Variable(labels)
@@ -72,13 +134,23 @@ for i in range(args.epochs):
             x = x.cuda()
             y = y.cuda()
         # print(x.data.shape, y.data.shape)
+        num_labels = y.data.size(1)
+        avg_length += x.data.size(1)
         model.train()
+        opti.zero_grad()
+        model.zero_grad()
         out = model(x)
         out = out.double()
         loss = crit(out, y)
         loss.backward()
         torch.nn.utils.clip_grad_norm(model.parameters(), 1.0)
         opti.step()
-        if j % 1 == 0:
-            print("Epoch: {}, Batch: {}/{}, loss: {}".format(i, j, data_size, loss))
-    evaluate(model, valid_loader, args.batchsize, crit, args.cuda)
+        if j % 50 == 0:
+            print("Epoch: {}, Batch: {}/{}, loss: {} average seq length: {}, data size: {}".format(
+                    i, j, data_size, loss.data[0], avg_length / float(j + 1), data_size * args.batchsize))
+    print("Evaluating model on training data")
+    evaluate(model, train_loader, crit, args.cuda, args.batchsize, num_labels)
+    print("Evaluating model on validation data")
+    evaluate(model, valid_loader, crit, args.cuda, args.batchsize, num_labels)
+    if i % 1 == 0:
+      torch.save(model.state_dict(), os.path.join(args.modelpath, args.experiment + '_' + str(i)))
