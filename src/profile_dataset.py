2326e8b27796513c9f99e414ec43466613584fdf
Laura Graesser <lhg256@crunchy6.cims.nyu.edu>
2017-12-10 19:36:01 -0500

    Profiler fixes

--- src/profile_dataset.py
+++ src/profile_dataset.py
@@ -2,7 +2,8 @@ import pickle
 import argparse
 import numpy as np
 from attention_databuilder import *
-
+PADDING = "<PAD>"
+UNKNOWN = "UNK"
 
 def count_unks(data):
     unks = 0
@@ -11,7 +12,7 @@ def count_unks(data):
         for sent in elem[1]:
             unks += sum([1 for _ in sent if _ == 'UNK'])
             toks += len(sent)
-    print("Toks: {} Unks: {} % UNKs{:.3f}", toks, unks, unks / float(toks))
+    print("Toks: {} Unks: {} % UNKs: {:.3f}".format(toks, unks, unks / float(toks)))
 
 
 def get_stats(data):
@@ -21,10 +22,10 @@ def get_stats(data):
         sents_per_note.append(len(elem[1]))
         for sent in elem[1]:
             words_per_sent.append(len(sent))
-    print("Avg sents: {:.3f}, Min sents: {}: Max sents: {}",
-          np.mean(sents_per_note), max(sents_per_note), min(sents_per_note))
-    print("Avg words: {:.3f}, Min words: {}: Max words: {}",
-          np.mean(words_per_sent), max(words_per_sent), min(words_per_sent))
+    print("Avg sents: {:.3f}, Min sents: {}: Max sents: {}".format(
+          np.mean(sents_per_note), min(sents_per_note), max(sents_per_note)))
+    print("Avg words: {:.3f}, Min words: {}: Max words: {}".format(
+          np.mean(words_per_sent), min(words_per_sent), max(words_per_sent)))
 
 
 parser = argparse.ArgumentParser(
@@ -33,6 +34,8 @@ parser.add_argument('--train_path', type=str,
                     default='/misc/vlgscratch2/LecunGroup/laura/medical_notes/processed_data/10codesL5_UNK_content_2_top1_train_data.pkl')
 parser.add_argument('--val_path', type=str,
                     default='/misc/vlgscratch2/LecunGroup/laura/medical_notes/processed_data/10codesL5_UNK_content_2_top1_valid_data.pkl')
+parser.add_argument('--vocab_threshold', type=int,
+                    default=5)
 args = parser.parse_args()
 print(args)
 
