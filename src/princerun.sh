b219d8aca2e6483f8f44a0efdf98c0c4dcb22e38
lgraesser <lhgraesser@gmail.com>
2017-12-14 23:56:00 -0500

    Prince run script

--- src/princerun.sh
+++ src/princerun.sh
@@ -20,4 +20,4 @@ python master_train_script.py --train_path /scratch/lhg256/nlp/mimic/50codesL5_U
 
 #python master_train_script.py --train_path /scratch/lhg256/nlp/mimic/50codesL5_UNK_content_4_top3_train_data.pkl --val_path /scratch/lhg256/nlp/mimic/50codesL5_UNK_content_4_top3_valid_data.pkl --model_dir /scratch/lhg256/nlp/mimic --attention 1 --num_workers 12 --embed_path /scratch/lhg256/nlp/stsp_model.tsv --num_epochs 15 --exp_name attention1_50_content4_top3_nostsp --use_starspace 0 --multilabel 1 --batch_size 8
 
-
+#python master_train_script.py --train_path /scratch/lhg256/nlp/mimic/50codesL5_UNK_content_4_top5_train_data.pkl --val_path /scratch/lhg256/nlp/mimic/50codesL5_UNK_content_4_top5_valid_data.pkl --model_dir /scratch/lhg256/nlp/mimic --attention 1 --num_workers 12 --embed_path /scratch/lhg256/nlp/stsp_model.tsv --num_epochs 15 --exp_name attention1_50_content4_top5_stsp --use_starspace 1 --multilabel 1 --batch_size 8
