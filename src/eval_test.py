f3cb71f0d05f88d022f2ac2ba8433f9c81681c8d
lgraesser <lhgraesser@gmail.com>
2017-12-16 21:02:29 -0500

    Fixing test set eval script

--- src/eval_test.py
+++ src/eval_test.py
@@ -26,8 +26,9 @@ from loss import *
 from sklearn.metrics import f1_score, precision_score, recall_score
 
 parser = argparse.ArgumentParser(description='MIMIC III notes data preparation')
+parser.add_argument('--exp_name', type=str, default='run')
 parser.add_argument('--train_path', type=str, default='/misc/vlgscratch2/LecunGroup/laura/medical_notes/processed_data/10codesL5_UNK_content_2_top1_train_data.pkl')
-parser.add_argument('--test_path', type=str, default='/misc/vlgscratch2/LecunGroup/laura/medical_notes/processed_data/10codesL5_UNK_content_2_top1_test_data.pkl')
+parser.add_argument('--val_path', type=str, default='/misc/vlgscratch2/LecunGroup/laura/medical_notes/processed_data/10codesL5_UNK_content_2_top1_valid_data.pkl')
 parser.add_argument('--model_path', type=str, default='/misc/vlgscratch2/LecunGroup/laura/medical_notes/models/test.pth')
 parser.add_argument('--attention', type=int, default=0)
 parser.add_argument('--cbow', type=int, default=0)
@@ -35,6 +36,12 @@ parser.add_argument('--batch_size', type=int, default=16)
 parser.add_argument('--num_workers', type=int, default=4)
 parser.add_argument('--embed_dim', type=int, default=50)
 parser.add_argument('--hidden_dim', type=int, default=100)
+parser.add_argument('--focalloss', type=int, default=0)
+parser.add_argument('--lr', type=float, default=1e-2)
+parser.add_argument('--lr_decay_rate', type=float, default=0.9)
+parser.add_argument('--lr_decay_epoch', type=int, default=10)
+parser.add_argument('--num_epochs', type=int, default=10)
+parser.add_argument('--log_interval', type=int, default=100)
 parser.add_argument('--vocab_threshold', type=int, default=20)
 parser.add_argument('--gpu_id', type=int, default=0)
 parser.add_argument('--build_starspace', type=int, default=0)
@@ -44,6 +51,7 @@ parser.add_argument('--embed_path', type=str, default="Starspace/stsp_model.tsv"
                     help='Where are the initialized embeddings?')
 args = parser.parse_args()
 print(args)
+
 
 PADDING = "<PAD>"
 UNKNOWN = "UNK"
@@ -55,31 +63,15 @@ if use_cuda:
     torch.cuda.set_device(args.gpu_id)
     #torch.backends.cudnn.enabled = False
 
-# data reader
-log_path = os.path.join('log', args.exp_name)
-if not os.path.exists(log_path):
-    os.makedirs(log_path)
-else:
-    exit("Log path already exists. Enter a new exp_name")
-
-args.model_dir = os.path.join(args.model_dir, args.exp_name)
-if not os.path.exists(args.model_dir):
-    os.makedirs(args.model_dir)
-
-tensorboard_logger.configure(log_path)
-
 
 ## MIMIC data code
 traindata = pickle.load(open(args.train_path, 'rb'))
-testdata = pickle.load(open(args.test_path, 'rb'))
+#count_labels(traindata, 50)
+valdata = pickle.load(open(args.val_path, 'rb'))
+#count_labels(valdata, 50)
 print("Train size:", len(traindata))
 print("Test size:", len(valdata))
 
-if args.build_starspace:
-    print("Building starspace embeddings. This will take a few minutes...")
-    stsp_data = convert_unflat_data_to_starspace_format(traindata)
-    write_starspace_format(stsp_data, "stsp_embeddings.txt")
-    subprocess.call(['./Starspace/run.sh'])
 
 label_map = {i:_ for _,i in enumerate(get_labels(traindata))}
 print(label_map)
@@ -97,38 +89,22 @@ print(label_map)
 print(list(token2idx.values())[:20])
 print("Label mix training data")
 count_labels(traindata)
-print("Label mix valid data")
-count_labels(testdata)
+print("Label mix test data")
+count_labels(valdata)
 num_labels = len(label_map)
 print("Number of labels is {}".format(num_labels))
-if args.use_starspace:
-    # Load starspace embeddings into a dict
-    #stsp_embed = load_starspace_embeds("Starspace/stsp_model.tsv", args.embed_dim)
-    stsp_embed = load_starspace_embeds(args.embed_path, args.embed_dim)
-    print(type(stsp_embed))
-    print("Embeddings loaded")
-    for i, k in enumerate(stsp_embed):
-        print(k, stsp_embed[k])
-        if i == 3:
-            break
-    # Create starspace embedding matrix
-    emb_mat = create_starspace_embedding_matrix(stsp_embed,
-                                                token2idx,
-                                                len(vocabulary),
-                                                args.embed_dim)
-    print("Embedding matrix created")
 
 one_hot = False
 if args.multilabel:
     print("Labels are one hot")
     one_hot = True
 trainset = NotesData(traindata, token2idx, UNKNOWN, label_map, one_hot)
-testset = NotesData(testdata, token2idx, UNKNOWN, label_map, one_hot)
+valset = NotesData(valdata, token2idx, UNKNOWN, label_map, one_hot)
 print("Data Loaded in %.2f mns."%((time.time()-_t)/60))
 
 train_loader = torch.utils.data.DataLoader(dataset = trainset, batch_size=args.batch_size, shuffle=True,
                                                            num_workers=args.num_workers, collate_fn=sent_batch_collate)
-val_loader = torch.utils.data.DataLoader(dataset = testset, batch_size=args.batch_size, shuffle=True,
+val_loader = torch.utils.data.DataLoader(dataset = valset, batch_size=args.batch_size, shuffle=True,
                                                            num_workers=args.num_workers, collate_fn=sent_batch_collate)
 print("data loader done")
 
@@ -170,8 +146,7 @@ if use_cuda:
         model.wordattention.context = model.wordattention.context.cuda()
         model.sentattention.context = model.sentattention.context.cuda()
 
-print("Evaluating on training set, multilabel eval")
-print("Number of labels is {}".format(num_labels))
-train_loss, train_acc, train_f1, train_precision, train_recall = eval_model_multi(model, train_loader, args.batch_size, crit, use_cuda, num_labels, args.batch_size)
-print("Evaluating on test set, multilabel eval")
-test_loss, test_acc, test_f1, test_precision, test_recall = eval_model_multi(model, test_loader, args.batch_size, crit, use_cuda, num_labels, args.batch_size)
+print("Evaluating on test set")
+val_loss, val_acc, val_f1, val_precision, val_recall = eval_model(model, val_loader, args.batch_size, crit, use_cuda)
+print("Evaluating on training set")
+train_loss, train_acc, train_f1, train_precision, train_recall = eval_model(model, train_loader, args.batch_size, crit, use_cuda)
